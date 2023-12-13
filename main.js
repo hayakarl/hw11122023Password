9d0324c00ffde4f1af168491f408d3a5e93ad999
haya karl <khayakyony@gmail.com>
2023-12-13 21:46:35 +0200

    t

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function checkLogin() {
     passwordBox.style.backgroundColor = "";
 
     if (usermail !== "idanmagl@gmail.com" || password !== "im1234$$") {
-        alert("Wrong usermail or password");
+        let showMessage = document.getElementById("messege")
+        showMessage.innerText = "Wrong usermail or password"
         usermailBox.style.backgroundColor = "red";
         usermailBox.focus();
         event.preventDefault();
